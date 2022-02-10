2f4c976b2b8a90f6420fe0d0e9bbca9d9954c383
abbymcinerney <abby.mcinerney@outlook.com>
2022-02-10 21:56:06 +0000

    organisation results

--- prototype-triad/app/routes.js
+++ prototype-triad/app/routes.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/test', function (req, res) {
+router.post('/service', function (req, res) {
 
     // Make a variable and give it the value from 'how-many-balls'
     var service = req.session.data['service']
   
     // Check whether the variable matches a condition
     if (service == "service1"){
-      // Send user to next page
       res.redirect('/homedelivery')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/start')
+    } else if(service == "service2"){
+      res.redirect('/collection_results')
+    }else{
+      res.redirect('/organisation_results')
     }
   
   })
